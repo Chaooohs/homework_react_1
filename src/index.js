6ae80b12d81defb51e52ae35576b8f228b48741a
Chaooohs <01nikka01@gmail.com>
2023-10-08 01:27:08 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Zodiak() {
+  return (
+    <table className='table'>
+      <tbody>
+        <tr>
+          <td className='one'>&#9800;</td>
+          <td className='one'>Овен</td>
+          <td className='one'>21 березня - 20 квітня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9801;</td>
+          <td className='one'>Телець</td>
+          <td className='one'>21 квітня - 21 травня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9802;</td>
+          <td className='one'>Близнюки</td>
+          <td className='one'>22 травня - 21 червня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9803;</td>
+          <td className='one'>Рак</td>
+          <td className='one'>22 червня - 22 липня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9804;</td>
+          <td className='one'>Лев</td>
+          <td className='one'>23 липня - 23 серпня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9805;</td>
+          <td className='one'>Діва</td>
+          <td className='one'>24 серпня - 22 вересня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9806;</td>
+          <td className='one'>Терези</td>
+          <td className='one'>23 вересня - 23 жовтня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9807;</td>
+          <td className='one'>Скорпіон</td>
+          <td className='one'>24 жовтня - 22 листопада</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9808;</td>
+          <td className='one'>Стрілець</td>
+          <td className='one'>23 листопада - 21 грудня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9809;</td>
+          <td className='one'>Козоріг</td>
+          <td className='one'>22 грудня - 20 січня</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9811;</td>
+          <td className='one'>Водолій</td>
+          <td className='one'>21 січня - 20 лютого</td>
+        </tr>
+        <tr>
+          <td className='one'>&#9934;</td>
+          <td className='one'>Риби </td>
+          <td className='one'>21 лютого - 20 березня</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Zodiak></Zodiak>, document.querySelector('#root'))
